37bde352aa47993c09e5d24657519036dcc7000f
Rusty Roberts <rust.roberts@protonmail.com>
2025-08-27 14:37:39 -0700

    feat: formatting

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,36 @@
+return {
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  keys = {
+    {
+      '<leader>f',
+      function()
+        require('conform').format({ async = true, lsp_format = 'fallback' })
+      end,
+      mode = '',
+      desc = '[F]ormat buffer',
+    },
+  },
+  opts = {
+    notify_on_error = false,
+    format_on_save = function(bufnr)
+      -- Disable with a global or buffer-local variable
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+      return { timeout_ms = 500, lsp_format = 'fallback' }
+    end,
+    formatters_by_ft = {
+      lua = { 'stylua' },
+      javascript = { 'prettier' },
+      typescript = { 'prettier' },
+      javascriptreact = { 'prettier' },
+      typescriptreact = { 'prettier' },
+      css = { 'prettier' },
+      html = { 'prettier' },
+      json = { 'prettier' },
+      python = { 'black' },
+    },
+  },
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -193,6 +193,7 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'prettier', -- Used to format HTML, CSS, JS, TS, TSX, JSON
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
