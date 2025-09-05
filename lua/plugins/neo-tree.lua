4d62bc9d973da1575ef133e01049faf5048ac2e4
Rusty Roberts <rust.roberts@protonmail.com>
2025-09-04 17:43:23 -0700

    chore: more neo-tree config wrangling

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -3,10 +3,19 @@ return {
   branch = 'v3.x',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'nvim-tree/nvim-web-devicons',
     'MunifTanjim/nui.nvim',
-    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
+  init = function()
+    -- refresh when gitsigns updates
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'GitSignsUpdate',
+      callback = function()
+        local events = require 'neo-tree.events'
+        events.fire_event(events.GIT_EVENT)
+      end,
+    })
+  end,
   keys = { -- Example mapping to toggle outline
     { '<leader>l', '<cmd>Neotree toggle<CR>', desc = 'Toggle neo-tree' },
   },
@@ -32,6 +41,21 @@ return {
         hide_gitignored = false,
       },
     },
+    default_component_configs = {
+      git_status = {
+        symbols = {
+          added = '+',
+          modified = '~',
+          deleted = '-',
+          renamed = '➜',
+          untracked = '?',
+          ignored = '◌',
+          unstaged = '●',
+          staged = '✓',
+          conflict = '!',
+        },
+      },
+    },
     git_status = {
       window = {
         position = 'float',
@@ -46,26 +70,10 @@ return {
         { source = 'git_status' },
       },
     },
-    event_handlers = {
-      {
-        event = 'git_status_changed',
-        handler = function()
-          require('neo-tree.sources.manager').refresh('filesystem')
-          require('neo-tree.sources.manager').refresh('git_status')
-        end,
-      },
-      {
-        event = 'file_added',
-        handler = function()
-          require('neo-tree.sources.manager').refresh('filesystem')
-        end,
-      },
-      {
-        event = 'file_deleted',
-        handler = function()
-          require('neo-tree.sources.manager').refresh('filesystem')
-        end,
-      },
+    commands = {
+      refresh_git = function(state)
+        require('neo-tree.sources.manager').refresh 'filesystem'
+      end,
     },
     auto_clean_after_session_restore = true,
   },
