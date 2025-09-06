768f3b4b5cbfbe2cbaf03eff1339170b8412a6f5
Rusty Roberts <rust.roberts@protonmail.com>
2025-09-06 11:57:34 -0700

    chore: transparent outline/tree

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,6 +5,11 @@ return {
     init = function()
       vim.cmd.colorscheme('tokyonight-night')
       vim.cmd.hi('Comment gui=none')
+
+      -- Make neo-tree and outline completely transparent
+      vim.cmd.hi('NeoTreeNormal guibg=NONE')
+      vim.cmd.hi('NeoTreeNormalNC guibg=NONE')
+      vim.cmd.hi('NeoTreeWinSeparator guibg=NONE')
     end,
   },
 }
