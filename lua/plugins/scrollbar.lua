9557645e79e50ede3260fe3fd3efd6a154647e5b
Rusty Roberts <rust.roberts@protonmail.com>
2025-09-06 18:09:28 -0700

    chore: tweak scrollbar

--- lua/plugins/scrollbar.lua
+++ lua/plugins/scrollbar.lua
@@ -3,9 +3,6 @@ return {
     'petertriho/nvim-scrollbar',
     event = 'BufReadPost',
     opts = {
-      handle = {
-        blend = 30,
-      },
       excluded_filetypes = {
         'cmp_docs',
         'cmp_menu',
