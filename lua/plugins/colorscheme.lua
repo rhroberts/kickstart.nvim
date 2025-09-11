c429d67af459c22854c83a3ee406d9a267627f5f
Rusty Roberts <rust.roberts@protonmail.com>
2025-09-11 12:31:51 -0700

    feat: moar transparency

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -10,6 +10,22 @@ return {
       vim.cmd.hi('NeoTreeNormal guibg=NONE')
       vim.cmd.hi('NeoTreeNormalNC guibg=NONE')
       vim.cmd.hi('NeoTreeWinSeparator guibg=NONE')
+
+      -- Make telescope windows transparent
+      vim.cmd.hi('TelescopeNormal guibg=NONE')
+      vim.cmd.hi('TelescopeBorder guibg=NONE')
+      vim.cmd.hi('TelescopePromptNormal guibg=NONE')
+      vim.cmd.hi('TelescopeResultsNormal guibg=NONE')
+      vim.cmd.hi('TelescopePreviewNormal guibg=NONE')
+      vim.cmd.hi('TelescopePromptBorder guibg=NONE')
+      vim.cmd.hi('TelescopeResultsBorder guibg=NONE')
+      vim.cmd.hi('TelescopePreviewBorder guibg=NONE')
+
+      -- Make which-key window transparent
+      vim.cmd.hi('WhichKey guibg=NONE')
+      vim.cmd.hi('WhichKeyNormal guibg=NONE')
+      vim.cmd.hi('WhichKeyFloat guibg=NONE')
+      vim.cmd.hi('NormalFloat guibg=NONE')
     end,
   },
 }
